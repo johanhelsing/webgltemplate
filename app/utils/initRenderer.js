76d1b288cefcea5d2a768d5ecddaeefd37604bf4
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2015-04-27 18:57:44 +0200

    Allow options for initRenderer.

--- app/utils/initRenderer.js
+++ app/utils/initRenderer.js
@@ -1,7 +1,7 @@
 var THREE = require('three');
 
-module.exports = containerEl => {
-    var renderer = new THREE.WebGLRenderer();
+module.exports = (containerEl, opts) => {
+    var renderer = new THREE.WebGLRenderer(opts);
     renderer.setSize(window.innerWidth, window.innerHeight);
     containerEl.appendChild(renderer.domElement);
     return renderer;
